61b3bf370f6d20db283f0d39bb612776691040c2
Justin <justintmitchell1991@gmail.com>
2018-06-16 20:41:58 -0700

    assignment 8 correction: duplicate todo

--- src/App.js
+++ src/App.js
@@ -2,29 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import ToDo from './components/ToDo.js';
-class ToDo extends React.Component {
-        deleteToDo(description) {
-          this.props.deleteToDo(description);
-        }
-          render() {
-            return (
-              <div className="wrapper">
-
-              <button className="deleteToDo" onClick = {(e) =>
-                  this.deleteToDo(this.props.description)}>Delete</button>
-                  {() => this.props.deleteToDo(this.props.description)}
-
-              <li>
-                <input type="checkbox" checked={ this.props.isCompleted }
-        onChange={ this.props.toggleComplete } />
-                <span>{ this.props.description }</span>
-              </li>
-              </div>
-            );
-          }
-        }
-
-    class App extends React.Component {
+class App extends React.Component {
           constructor(props) {
             super(props);
             this.state = {
@@ -83,3 +61,5 @@ class ToDo extends React.Component {
             );
           }
         }
+
+export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 
-class ToDo extends Component {
-  deleteToDo(description) {
-    this.props.deleteToDo(description);
-  }
+class ToDo extends React.Component {
+        deleteToDo(description) {
+          this.props.deleteToDo(description);
+        }
+          render() {
+            return (
+              <div className="wrapper">
 
-class ToDo extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <button
-          className="deleteToDo"
-          onClick={e => this.deleteToDo(this.props.description)}
-        >
-          Delete
-        </button>
-        {this.props.deleteToDo}
+              <button className="deleteToDo" onClick = {(e) =>
+                  this.deleteToDo(this.props.description)}>Delete</button>
+                  {() => this.props.deleteToDo(this.props.description)}
 
-      <li>
-        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-        <span>{ this.props.description }</span>
-      </li>
-    );
-  }
-}
+              <li>
+                <input type="checkbox" checked={ this.props.isCompleted }
+        onChange={ this.props.toggleComplete } />
+                <span>{ this.props.description }</span>
+              </li>
+              </div>
+            );
+          }
+        }
 
 export default ToDo;
